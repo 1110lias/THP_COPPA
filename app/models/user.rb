d50e945c9d8f930d1c250f5d5730f5084a1766e3
1110lias <elias-b@outlook.fr>
2024-05-28 22:47:49 +0200

    avant pull pour recupérer updated layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
   has_many :orders
   has_one :cart
   validates :email, presence: true, uniqueness: true
-  validates :phone_number, presence: true, uniqueness: true
 end
