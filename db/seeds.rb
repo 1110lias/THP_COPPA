67e0b084f758619c5c7825b92b6fce150f336a01
Nzoops <Ton adresse mail githubls>
2024-05-28 12:20:00 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+
+Cart.destroy_all
+Order.destroy_all
+Product.destroy_all
+User.destroy_all
+
+#Create users
+users =[]
+10.times do
+    user = User.create(
+        first_name: Faker::Name.first_name, 
+        last_name: Faker::Name.last_name, 
+        password_digest: Faker::Name.last_name, 
+        email: Faker::Internet.email, 
+        phone_number: Faker::PhoneNumber.phone_number, 
+        billing_address: Faker::Address.full_address, 
+        delivery_address: Faker::Address.full_address)
+end
+
+#Create fake carts
+carts = []
+10.times do
+    cart = Cart.create!(
+        total_price: random(1..1000), 
+        user_id: random(1..100), 
+        created_at: Time.now - rand(1..30).days,
+        updated_at: Time.now - rand(1..30).days)
+end
+
+#Create fake products
+products = []
+10.times do 
+    product = Product.create(
+        title: 
+    )
